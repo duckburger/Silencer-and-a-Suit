449c1a7826d969b18ba05428bceb0ae6beaf1635
Kirill M <kmuravskiy@gmail.com>
2018-07-02 18:57:07 -0400

    Init commit

---
+++ Assets/Camera/SmoothCameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour {
+
+    [SerializeField] Camera currentCamera;
+    [SerializeField] Transform currentTarget;
+    [SerializeField] float smoothing;
+
+
+    private void Update()
+    {
+        Vector3 adjustedCharPos = new Vector3(currentTarget.transform.position.x, currentTarget.transform.position.y, -10f);
+        Vector3 newCamPos = Vector2.Lerp(currentCamera.transform.position, adjustedCharPos, Time.time * smoothing);
+        currentCamera.transform.position = new Vector3(newCamPos.x, newCamPos.y, -10f);
+    }
+
+
+}
---
+++ Assets/Editor/SpriteImportSettings.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+public class SpriteImportSettings : AssetPostprocessor {
+
+
+    void OnPreprocessTexture()
+    {
+        TextureImporter importer = (TextureImporter)assetImporter;
+        importer.textureType = TextureImporterType.Sprite;
+        importer.spriteImportMode = SpriteImportMode.Multiple;
+        importer.spritePixelsPerUnit = 100;
+        importer.mipmapEnabled = false;
+        importer.filterMode = FilterMode.Point;
+        importer.textureCompression = TextureImporterCompression.Uncompressed;
+    }
+	
+}
