00cb50cf52481d3d6554529632351b9f518a12fa
Kirill M <kmuravskiy@gmail.com>
2018-07-23 22:04:05 -0400

    Added a basic door that opens and closes.

---
+++ Assets/Environments/Indoor Items/Doors/FingerprintDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerprintDoor : MonoBehaviour {
+
+    [SerializeField] SpriteRenderer mySpriteRenderer;
+
+    bool isOpen = false;
+
+    public void ToggleDoor()
+    {
+        if (isOpen)
+        {
+            LeanTween.cancel(this.gameObject);
+            Close();
+        }
+        else
+        {
+            LeanTween.cancel(this.gameObject);
+            Open();
+        }
+    }
+
+    void Open()
+    {
+        LeanTween.moveLocalY(this.gameObject, this.transform.localPosition.y + mySpriteRenderer.sprite.rect.height / 32, 0.3f);
+        LeanTween.alpha(this.gameObject, 0, 0.3f);
+        isOpen = true;
+    }
+
+    void Close()
+    {
+        LeanTween.moveLocalY(this.gameObject, this.transform.localPosition.y - mySpriteRenderer.sprite.rect.height / 32, 0.3f);
+        LeanTween.alpha(this.gameObject, 1, 0.3f);
+        isOpen = false;
+    }
+
+}
---
+++ Assets/NPCs/Scripts/EnemyAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    bool detectedPlayer;
+
+    private void Update()
+    {
+       
+    }
+
+    void Movement()
+    {
+
+    }
+
+}
